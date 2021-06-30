aef13993d19df99a8819e4036d65eb02272383ce
samuel.mazilu@microfocus.com <samuel.mazilu@microfocus.com>
2021-06-30 16:39:36 +0300

    App bootstrap template.

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    var forms = require('forms');
+    var fields = forms.fields;
+    var validators = forms.validators;
+
+    var reg_form = forms.create({
+        username: fields.string({ required: true }),
+        password: fields.password({ required: validators.required('You definitely want a password') }),
+        confirm:  fields.password({
+            required: validators.required('don\'t you know your own password?'),
+            validators: [validators.matchField('password')]
+        }),
+        email: fields.email()
+    });
+    res.send(reg_form.toHTML());
+});
+
+app.post('/submit-student-data', function (req, res) {
+    var name = req.body.firstName + ' ' + req.body.lastName;
+    
+    res.send(name + ' Submitted Successfully!');
+});
+
+var server = app.listen(80, function () {
+    console.log('Node server is running..');
+});
