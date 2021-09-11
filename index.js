20468645837045042000364b4611b3ab60cc9a0b
samuel.mazilu <samuel.mazilu@microfocus.com>
2021-09-11 21:16:08 +0300

    testing recaptcha

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var forms = require('forms');
 var bodyParser = require("body-parser");
+const { default: createAssessment } = require('./lib/google');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static('public'))
 //test
@@ -10,9 +11,9 @@ app.use('/', express.static('public'))
 //     res.sendFile('./public/index.html', { root: __dirname });
 // });
 
-app.post('/submit-student-data', function (req, res) {
+app.post('/submit', function (req, res) {
     var name = req.body.firstName + ' ' + req.body.lastName;
-    
+    createAssessment(req.body.g_token, 'homepage', () => console.log('ok'), ()=>console.log('error'));
     res.send(name + ' Submitted Successfully!');
 });
 
