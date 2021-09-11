5e3f4655d797e9080d3fd71aaf576fdce3ded223
samuel.mazilu@microfocus.com <samuel.mazilu@microfocus.com>
2021-09-11 18:51:19 +0300

    Added sampe form.

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 var express = require('express');
 var app = express();
-
+var forms = require('forms');
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/static', express.static('public'))
 //test
-app.get('/', function (req, res) {
-    var forms = require('forms');
-    var fields = forms.fields;
-    var validators = forms.validators;
 
-    var reg_form = forms.create({
-        username: fields.string({ required: true }),
-        password: fields.password({ required: validators.required('You definitely want a password') }),
-        confirm:  fields.password({
-            required: validators.required('don\'t you know your own password?'),
-            validators: [validators.matchField('password')]
-        }),
-        email: fields.email()
-    });
-    res.send(reg_form.toHTML());
+app.get('/', function (req, res) {
+    res.sendFile('./public/index.html', { root: __dirname });
 });
 
 app.post('/submit-student-data', function (req, res) {
