da2d588d13650b1a79ebbd429b1ac96cfb120712
samuel.mazilu <samuel.mazilu@microfocus.com>
2021-09-11 22:07:37 +0300

    removed comment

--- index.js
+++ index.js
@@ -5,17 +5,12 @@ var bodyParser = require("body-parser");
 const createAssessment = require('./lib/google');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static('public'))
-//test
 
-// app.get('/', function (req, res) {
-//     res.sendFile('./public/index.html', { root: __dirname });
-// });
 
 app.post('/submit', function (req, res) {
     createAssessment(req.body.g_token, 'homepage', () => console.log('ok'), ()=>console.log('error'));
     res.send(' Submitted Successfully!');
 });
-
 var server = app.listen(8080, function () {
     console.log('Node server is running..');
 });
