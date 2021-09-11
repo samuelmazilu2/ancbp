48b3380144770e7f303501a260febc5464366036
samuel.mazilu <samuel.mazilu@microfocus.com>
2021-09-11 20:08:59 +0300

    changed static path

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var app = express();
 var forms = require('forms');
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/static', express.static('public'))
+app.use('/', express.static('public'))
 //test
 
-app.get('/', function (req, res) {
-    res.sendFile('./public/index.html', { root: __dirname });
-});
+// app.get('/', function (req, res) {
+//     res.sendFile('./public/index.html', { root: __dirname });
+// });
 
 app.post('/submit-student-data', function (req, res) {
     var name = req.body.firstName + ' ' + req.body.lastName;
