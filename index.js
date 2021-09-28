020e6b654f72f5aa31880e51a030314c504da8d4
samuel.mazilu <samuel.mazilu@microfocus.com>
2021-09-28 22:51:30 +0300

    Added rate limiter.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var express = require('express');
-var app = express();
-var forms = require('forms');
-var bodyParser = require("body-parser");
+const express = require('express');
+const RateLimit = require('express-rate-limit');
+const app = express();
+const forms = require('forms');
+const bodyParser = require("body-parser");
+const path = require('path');
 const createAssessment = require('./lib/google');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static('public'))
 
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 60
+});
 app.get('/vcard', (req, res) => {
     const fileDirectory = path.resolve(__dirname, '.', 'public/');
 
