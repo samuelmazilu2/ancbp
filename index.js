2b8057dc85e008be9fd78ca28272d6b7d10c13e8
samuel.mazilu <samuel.mazilu@microfocus.com>
2021-09-28 22:38:49 +0300

    added vcard

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const createAssessment = require('./lib/google');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static('public'))
 
+app.get('/vcard', (req, res) => {
+    const fileDirectory = path.resolve(__dirname, '.', 'public/');
 
+    res.sendFile('vcard.vcf', {root: fileDirectory}, (err) => {
+      res.end();
+  
+      if (err) throw(err);
+    });
+});
 app.post('/submit', function (req, res) {
     createAssessment(req.body.g_token, 'homepage', () => console.log('ok'), ()=>console.log('error'));
     res.send(' Submitted Successfully!');
@@ -14,3 +22,4 @@ app.post('/submit', function (req, res) {
 var server = app.listen(8080, function () {
     console.log('Node server is running..');
 });
+
