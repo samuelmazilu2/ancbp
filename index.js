afe2d0353f8bfe164b9bef76bad0b2791267f1df
samuel.mazilu <samuel.mazilu@microfocus.com>
2021-11-13 20:47:05 +0200

    save form result in sqllite db

--- index.js
+++ index.js
@@ -7,23 +7,29 @@ const path = require('path');
 const createAssessment = require('./lib/google');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static('public'))
+const { body, validationResult } = require('express-validator');
+const { createNewForm } = require('./lib/data');
 
 var limiter = new RateLimit({
   windowMs: 1*60*1000, // 1 minute
-  max: 60
+  max: process.env.MAX_REQUESTS_PER_MINUTE
 });
 app.get('/vcard', (req, res) => {
     const fileDirectory = path.resolve(__dirname, '.', 'public/');
-
     res.sendFile('vcard.vcf', {root: fileDirectory}, (err) => {
-      res.end();
-  
+      res.end(); 
       if (err) throw(err);
     });
 });
-app.post('/submit', function (req, res) {
+app.post('/submit', body('name').isLength({ min: 5 }), body('email').isEmail(), body('message').isLength({min:100}), async(req, res) => {
     createAssessment(req.body.g_token, 'homepage', () => console.log('ok'), ()=>console.log('error'));
-    res.send(' Submitted Successfully!');
+    await createNewForm({
+      name: req.body.name,
+      email: req.body.email,
+      message: req.body.message,
+      location: req.body.location
+    })
+    res.send('Submitted Successfully!');
 });
 var server = app.listen(8080, function () {
     console.log('Node server is running..');
