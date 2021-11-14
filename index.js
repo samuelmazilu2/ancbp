03ad50e38d2a7d54f1b03a64f5569fb67c77bdd9
samuel.mazilu <samuel.mazilu@microfocus.com>
2021-11-14 10:22:16 +0200

    added global exception handling

--- index.js
+++ index.js
@@ -11,28 +11,37 @@ const { body, validationResult } = require('express-validator');
 const { createNewForm } = require('./lib/data');
 const logger = require('./lib/logger');
 
-var limiter = new RateLimit({
-  windowMs: 1*60*1000, // 1 minute
-  max: process.env.MAX_REQUESTS_PER_MINUTE
-});
-app.get('/vcard', (req, res) => {
-    const fileDirectory = path.resolve(__dirname, '.', 'public/');
-    res.sendFile('vcard.vcf', {root: fileDirectory}, (err) => {
-      res.end(); 
-      if (err) throw(err);
-    });
-});
-app.post('/submit', body('name').isLength({ min: 5 }), body('email').isEmail(), body('message').isLength({min:100}), async(req, res) => {
-    createAssessment(req.body.g_token, 'homepage', () => console.log('ok'), ()=>console.log('error'));
-    await createNewForm({
-      name: req.body.name,
-      email: req.body.email,
-      message: req.body.message,
-      location: req.body.location
-    })
-    res.send('Submitted Successfully!');
-});
-var server = app.listen(8080, function () {
-    logger.info('Node server is running..');
-});
-
+try {
+  var limiter = new RateLimit({
+    windowMs: 1*60*1000, // 1 minute
+    max: process.env.MAX_REQUESTS_PER_MINUTE
+  });
+  app.get('/vcard', (req, res) => {
+      const fileDirectory = path.resolve(__dirname, '.', 'public/');
+      res.sendFile('vcard.vcf', {root: fileDirectory}, (err) => {
+        res.end(); 
+        if (err) throw(err);
+      });
+  });
+  app.get('/health', (req, res) => res.status(200).send("ok"));
+  app.post('/submit', body('name').isLength({ min: 5 }), body('email').isEmail(), body('message').isLength({min:100}), async(req, res) => {
+      logger.info('Start /submit')
+      createAssessment(req.body.g_token, 'homepage', () => console.log('ok'), ()=>console.log('error'));
+      await createNewForm({
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        location: req.body.location
+      })
+      res.send('Submitted Successfully!');
+  });
+  var server = app.listen(8080, function () {
+      logger.info('Node server is running..');
+  });
+  
+  
+}
+catch(ex){
+  logger.error("error encountered in main loop!");
+  logger.error(ex);
+}
